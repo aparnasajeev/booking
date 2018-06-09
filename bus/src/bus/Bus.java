30c1bbf91534a5b1dc81028860a19b1326aad1fe
Shantanu <Shantanu@192.168.1.110>
2018-06-09 13:03:00 +0530

    details

--- bus/src/bus/Bus.java
+++ bus/src/bus/Bus.java
@@ -16,6 +16,8 @@ public class Bus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+          details d1=new details();
     }
     
 }
